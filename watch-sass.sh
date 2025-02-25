77ac851808900150942acfcbf93c05fe2f2e3c70
accrazed <accrazed@proton.me>
2025-02-24 21:42:53 -0800

    chore :build out release br

--- watch-sass.sh
+++ watch-sass.sh
@@ -1,5 +1,5 @@
-sass --no-source-map -w src/NieR-Import.theme.scss:NieR-Import.theme.css \
-        src/dark.scss:DarkImport.theme.css \
-        src/light.scss:LightImport.theme.css \
-        src/night.scss:NightImport.theme.css \
-        src/dev.theme.scss:$1/dev.theme.css
+sass --no-source-map -w src2/NieR-Import.theme.scss:NieR-Import.theme.css \
+        src2/dark.scss:DarkImport.theme.css \
+        src2/light.scss:LightImport.theme.css \
+        src2/night.scss:NightImport.theme.css \
+        src2/dev.theme.scss:$1/dev.theme.css
