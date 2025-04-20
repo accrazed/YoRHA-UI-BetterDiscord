c53e2618f0d3fcf60da3d4019b95369f5c96eb83
accrazed <accrazed@proton.me>
2025-04-20 11:10:01 -0700

    fuckin ?? test lol lmao idk

--- watch-sass.bat
+++ watch-sass.bat
@@ -1,7 +1,3 @@
 @echo off
 set themeDir=%1
-sass --no-source-map -w src/NieR-Import.theme.scss:NieR-Import.theme.css ^
-     src/dark.scss:DarkImport.theme.css ^
-     src/light.scss:LightImport.theme.css ^
-     src/night.scss:NightImport.theme.css ^
-     src/dev.theme.scss:"%themeDir%\dev.theme.css"
+sass --no-source-map -w src/dev.theme.scss:"%themeDir%\dev.theme.css"
