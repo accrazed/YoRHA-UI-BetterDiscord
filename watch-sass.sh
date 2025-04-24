70eabb45735e22f69db7933a26acd45b3827133e
accrazed <accrazed@proton.me>
2025-04-23 19:43:01 -0700

    fix(meccaxp): vencord panels

--- watch-sass.sh
+++ watch-sass.sh
@@ -1,1 +1,5 @@
-sass --no-source-map -w src/dev.theme.scss:$1/dev.theme.css
+sass --no-source-map -w src/NieR-Import.theme.scss:NieR-Import.theme.css \
+                         src/dark.scss:DarkImport.theme.css \
+                         src/light.scss:LightImport.theme.css \
+                         src/night.scss:NightImport.theme.css \
+                         src/dev.theme.scss:$1/dev.theme.css
